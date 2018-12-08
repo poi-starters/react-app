efcfdfcbfb9f70afc024fe53f468892e7cdb2f42
EGOIST <0x142857@gmail.com>
2018-12-08 18:50:09 +0800

    replace react-scripts with poi

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { hot } from 'react-hot-loader';
 
 class App extends Component {
   render() {
@@ -25,4 +26,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default hot(module)(App);
